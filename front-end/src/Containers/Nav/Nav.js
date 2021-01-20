00152a816c1e974bdf9f9469ac892f0fdbc39ff6
mateusz <mateusz.alicante@gmail.com>
2021-01-20 20:19:33 +0100

    added nav for mobile

--- front-end/src/Containers/Nav/Nav.js
+++ front-end/src/Containers/Nav/Nav.js
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styles from './Nav.module.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faTimes, faShoppingCart, faHome, faAddressCard, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
+
+    const [menuOpen, setOpen] = useState(false)
+
+    useEffect(() => console.log(menuOpen))
+
+
     return (
         <div>
             <div className={styles.navHeader}>
                 <h1>Site Name</h1>
                 <nav className={styles.navContainer}>
-                    <a >About</a>
-                    <a >Services</a>
-                    <a >Contact</a>
+                    <a>Home</a>
+                    <a>Store</a>
+                    <a>About</a>
+                    <a>My account</a>
                 </nav>
-                <nav className={styles.mobileNavHeader}>
-                    <FontAwesomeIcon icon={faBars} />
+                <nav className={styles.mobileNavHeader} onClick={() => setOpen(!menuOpen)}>
+                    <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+                </nav>
+            </div>
+            <div className={styles.navMobileContainer} style={{height: menuOpen ? "10em" : "0", opacity: menuOpen ? "100%" : "0"}}>
+                <nav className={styles.navMobileInnerContainer}>
+                    <a><FontAwesomeIcon icon={faHome} /> Home</a>
+                    <a><FontAwesomeIcon icon={faShoppingCart} /> Store</a>
+                    <a><FontAwesomeIcon icon={faInfoCircle} /> About</a>
+                    <a><FontAwesomeIcon icon={faAddressCard} /> My account</a>
                 </nav>
             </div>
         </div>
